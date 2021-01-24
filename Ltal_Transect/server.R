427cf7ebf3dfc55e74bc90797fa08ea1558dd2fd
Patrick Fonti <patrick.fonti@wsl.ch>
2021-01-24 20:26:42 +0100

    Some adjustment for the shiny (including now temperature, Battery and sapflow) AND example of markdown report

--- Ltal_Transect/server.R
+++ Ltal_Transect/server.R
@@ -2,80 +2,167 @@
 # This is to plot data from Campbell logger in the Loetchental.
 #
 
-library(shiny)
 
 # Define server logic required to draw a dygraph plot
 # Define server function
 server <- function(input, output) {
-  
-  # Subset data
-  selected_dendro <- reactive({
+  selected_batt <- reactive({
     req(input$date)
     validate(need(!is.na(input$date[1]) & !is.na(input$date[2]), "Error: Please provide both a start and an end date."))
     validate(need(input$date[1] < input$date[2], "Error: Start date should be earlier than end date."))
     
-    SITE <- DATA[grep(input$site,names(DATA))] %>% 
+    SITE.long.temp <- DATA[grep(input$site,names(DATA))] %>% 
       as.data.frame(col.names = "") %>%
       mutate(TIMESTAMP = as.POSIXct(TIMESTAMP)) %>%
-      filter(TIMESTAMP > as.POSIXct(input$date[1]) & TIMESTAMP < as.POSIXct(input$date[2]))
+      mutate_at(grep("Avg",colnames(.)),funs(as.character)) %>%
+      mutate_at(grep("Avg",colnames(.)),funs(as.numeric)) %>%
+      filter(TIMESTAMP > as.POSIXct(input$date[1]) & TIMESTAMP < as.POSIXct(input$date[2])) %>% 
+      select(c(TIMESTAMP, starts_with("Batt_Volt"))) %>%
+      pivot_longer(starts_with("Batt_Volt"), names_to = 'Sensor') %>%
+      mutate(value = as.numeric(as.character(value)), Sensor = factor(Sensor)) %>%
+      as.data.frame()
   })
    
-  selected_site <- reactive({
+  selected_temp <- reactive({
+    req(input$date)
+    validate(need(!is.na(input$date[1]) & !is.na(input$date[2]), "Error: Please provide both a start and an end date."))
+    validate(need(input$date[1] < input$date[2], "Error: Start date should be earlier than end date."))
+    
+    SITE.long.temp <- DATA[grep(input$site,names(DATA))] %>% 
+      as.data.frame(col.names = "") %>%
+      mutate(TIMESTAMP = as.POSIXct(TIMESTAMP)) %>%
+      mutate_at(grep("Avg",colnames(.)),funs(as.character)) %>%
+      mutate_at(grep("Avg",colnames(.)),funs(as.numeric)) %>%
+      filter(TIMESTAMP > as.POSIXct(input$date[1]) & TIMESTAMP < as.POSIXct(input$date[2])) %>% 
+      select(c(TIMESTAMP, starts_with("PanelTemp"))) %>%
+      pivot_longer(starts_with("PanelTemp"), names_to = 'Sensor') %>%
+      mutate(value = as.numeric(as.character(value)), Sensor = factor(Sensor)) %>%
+      as.data.frame()
+  })
+
+    selected_site <- reactive({
       req(input$date)
       validate(need(!is.na(input$date[1]) & !is.na(input$date[2]), "Error: Please provide both a start and an end date."))
       validate(need(input$date[1] < input$date[2], "Error: Start date should be earlier than end date."))
       
-    SITE.long <- DATA[grep(input$site,names(DATA))] %>% 
+    SITE.long.dendro <- DATA[grep(input$site,names(DATA))] %>% 
       as.data.frame(col.names = "") %>%
       mutate(TIMESTAMP = as.POSIXct(TIMESTAMP)) %>%
+      mutate_at(grep("Avg",colnames(.)),funs(as.character)) %>%
+      mutate_at(grep("Avg",colnames(.)),funs(as.numeric)) %>%
       filter(TIMESTAMP > as.POSIXct(input$date[1]) & TIMESTAMP < as.POSIXct(input$date[2])) %>% 
       select(c(TIMESTAMP, starts_with("Dendr"))) %>%
       pivot_longer(starts_with("Dendr"), names_to = 'Sensor') %>%
-      mutate(value = as.numeric(as.character(value)), Sensor = as.factor(Sensor)) %>%
+      mutate(value = as.numeric(as.character(value)), Sensor = factor(Sensor)) %>%
       as.data.frame() %>%
       group_by(Sensor) %>% 
-      mutate(value = value - mean(value))
-  })
+      mutate(value = value - mean(value)) %>% 
+      ungroup() %>% as.data.frame()
+      })
+    
+    # SITE.long <- SITE %>%
+    #   select(c(TIMESTAMP, starts_with("Dendr"))) %>%
+    #   pivot_longer(starts_with("Dendr"), names_to = 'Sensor') %>%
+    #   mutate(value = as.numeric(as.character(value)), Sensor = as.factor(Sensor)) %>%
+    #   as.data.frame() %>%
+    #   group_by(Sensor) %>%
+    #   mutate(value = value - mean(value))
 
-  # SITE <- DATA[grep('N08b_',names(DATA))] %>%
-  #   as.data.frame(col.names = "") %>%
-  #   mutate(TIMESTAMP = as.POSIXct(TIMESTAMP)) %>%
-  #   filter(TIMESTAMP > as.POSIXct('2020-01-01') & TIMESTAMP < as.POSIXct('2021-12-31'))
-  # 
-  # 
-  #   SITE.long <- SITE %>%
-  #     select(c(TIMESTAMP, starts_with("Dendr"))) %>%
-  #     pivot_longer(starts_with("Dendr"), names_to = 'Sensor') %>%
-  #     mutate(value = as.numeric(as.character(value)), Sensor = as.factor(Sensor)) %>%
-  #     as.data.frame() %>%
-  #     group_by(Sensor) %>% 
-  #     mutate(value = value - mean(value))
-  #     
-      
-  
-  # Create scatterplot object the plotOutput function is expecting
-  output$lineplot <- renderPlot({
-    color = 'white' #"#434343"
-    par(mar = c(4, 4, 1, 1))
-    plot(x = selected_dendro()$TIMESTAMP, y = selected_dendro()$Dendr_Avg.1., type = "l",
-      xlab = "Date", ylab = "Delta R", col = color, fg = color, col.lab = color, col.axis = color)
-    # Display only if smoother is checked
-    if(input$smoother){
-      smooth_curve <- lowess(x = as.numeric(selected_dendro()$TIMESTAMP), y = selected_dendro()$Dendr_Avg.1., f = input$f)
-      lines(smooth_curve, col = "#E6553A", lwd = 3)
-    }
+  selected_sap <- reactive({
+    req(input$date)
+    validate(need(!is.na(input$date[1]) & !is.na(input$date[2]), "Error: Please provide both a start and an end date."))
+    validate(need(input$date[1] < input$date[2], "Error: Start date should be earlier than end date."))
+    
+    SITE.long.sap <- DATA[grep(input$site,names(DATA))] %>% 
+      as.data.frame(col.names = "") %>%
+      mutate(TIMESTAMP = as.POSIXct(TIMESTAMP)) %>%
+      mutate_at(grep("Avg",colnames(.)),funs(as.character)) %>%
+      mutate_at(grep("Avg",colnames(.)),funs(as.numeric)) %>%
+      filter(TIMESTAMP > as.POSIXct(input$date[1]) & TIMESTAMP < as.POSIXct(input$date[2])) %>% 
+      select(c(TIMESTAMP, starts_with("Granier"))) %>%
+      pivot_longer(starts_with("Granier"), names_to = 'Sensor') %>%
+      mutate(value = as.numeric(as.character(value)), Sensor = factor(Sensor)) %>%
+      as.data.frame() %>%
+      group_by(Sensor) %>% 
+      mutate(value = value - mean(value)) %>% 
+      ungroup() %>% as.data.frame()
   })
 
   # Create ggplot object the plotOutput function is expecting
-  output$ggplot <- renderPlot({
-   if(input$smoother){
-     ggplot(selected_site(), aes(x=TIMESTAMP,y=value, colour=Sensor)) +
-       geom_line() +
-       geom_smooth(span = input$f)
-   } else {
-     ggplot(selected_site(), aes(x=TIMESTAMP,y=value, colour=Sensor)) +
-       geom_line()
-         }
-  })
+  output$ggplot.Batt <- renderPlot({
+    withProgress(message = 'Calculation in progress',
+      detail = 'This may take a while...', value = 0, {
+        for (i in 1:5) {
+          incProgress(1/5)
+          Sys.sleep(0.1)
+        }
+      })
     
-}
+      ggplot(selected_batt(), aes(x=TIMESTAMP,y=value, colour=Sensor)) + 
+        geom_line() + ylab("Voltage") + xlab("") + theme(legend.position="bottom")
+    })
+
+  # Create ggplot object the plotOutput function is expecting
+  output$ggplot.Temp <- renderPlot({
+       ggplot(selected_temp(), aes(x=TIMESTAMP,y=value, colour=Sensor)) + 
+        geom_line() + ylab("Temp") + xlab("") + theme(legend.position="bottom")
+   })
+
+    # Create ggplot object the plotOutput function is expecting
+  output$ggplot.Dendro <- renderPlot({
+     ggplot(selected_site(), aes(x=TIMESTAMP,y=value, colour=Sensor)) + 
+       geom_line() + ylab("DeltaR") + xlab("") + theme(legend.position="bottom")
+  })
+  
+  
+  # Create ggplot object the plotOutput function is expecting
+  output$ggplot.Sapflow <- renderPlot({
+      ggplot(selected_sap(), aes(x=TIMESTAMP,y=value, colour=Sensor)) +
+        geom_line() + ylab("Sap density") + xlab("") + theme(legend.position="bottom")
+  })
+
+  }
+
+  #   ########
+
+
+
+# # Create Dygraph object the plotOutput function is expecting
+# output$dygraph <- renderDygraph({
+#   # selected_DY() %>% 
+#   dygraph(selected_DY()) %>% dyRangeSelector(height=50) %>%
+#     dyOptions(fillGraph = TRUE) 
+# })
+
+# selected_DY <- reactive({
+#   req(input$date)
+#   validate(need(!is.na(input$date[1]) & !is.na(input$date[2]), "Error: Please provide both a start and an end date."))
+#   validate(need(input$date[1] < input$date[2], "Error: Start date should be earlier than end date."))
+#   
+#   SITE.DG %>% DATA[grep(input$site,names(DATA))] %>% 
+#     as.data.frame(col.names = "") %>%
+#     mutate(TIMESTAMP = as.POSIXct(TIMESTAMP)) %>%
+#     mutate_at(grep("Avg",colnames(.)),funs(as.character)) %>%
+#     mutate_at(grep("Avg",colnames(.)),funs(as.numeric)) %>%
+#     filter(TIMESTAMP > as.POSIXct(input$date[1]) & TIMESTAMP < as.POSIXct(input$date[2])) %>% 
+#     select(c('Dendr_Avg.1.', 'Dendr_Avg.2.','Dendr_Avg.3.', 'Dendr_Avg.4.')) %>%
+#     xts(SITE[,-1:-2], order.by=SITE[,1])
+#   
+#   print(head(SITE.DG)) 
+# })
+# 
+# SITE %>%
+#  select(c('Dendr_Avg.1.', 'Dendr_Avg.2.','Dendr_Avg.3.', 'Dendr_Avg.4.')) %>%
+#  xts(SITE[,-1:-2], order.by=SITE[,1]) %>%
+#  dygraph() %>% dyRangeSelector(height=50) %>%
+#  dyOptions(fillGraph = TRUE) %>%
+#  dyShading(from = "1912-1-1", to = "2021-1-1", color = "black")
+
+    
+    
+    
+    
+    
+    
+    
+    
--- Ltal_Transect/ui.R
+++ Ltal_Transect/ui.R
@@ -2,13 +2,17 @@
 # This is to plot data from Campbell logger in the Loetchental. 
 #
 
+# remotes::install_github("rstudio/bslib")
+# remotes::install_github("rstudio/shiny")
+# remotes::install_github("rstudio/thematic")
+# remotes::install_github("rstudio/dygraphs")
+
 # Load packages
 library(shiny)
 library(shinythemes)
 library(bslib)
 library(thematic)
 library(ggplot2)
-# library(dygraphs)
 
 # add bslib thematic
 material <- bs_theme(
@@ -20,55 +24,63 @@ material <- bs_theme(
   info = "#28B3ED",
   warning = "#FD7424",
   danger = "#F7367E",
-  base_font = font_google("Open Sans"),
-  heading_font = font_google("Proza Libre"),
-  code_font = font_google("Fira Code")
+  base_font = font_google("Indie Flower"), #"Open Sans"
+  heading_font = font_google("Indie Flower"), #"Proza Libre"
+  code_font = font_google("Indie Flower") #"Fira Code"
 )
 # bs_theme_preview(material, with_themer = TRUE)
 
 # Add thematic for ggplot
-thematic_on(bg = '#202123', fg = 'white', accent = 'red', font = NA)
+thematic_on(bg = '#202123', fg = 'white', accent = 'red', font = "Indie Flower")
 
 
 
 # Load data
 #source('1.Import_FTP_data.R')
 #DATA <- DATA
-head(DATA)
+#head(DATA)
+
+extract <- function(x) {unlist(strsplit((x),'_'))[[2]]}
 
 # Define UI for application that draws a Dygraph plot
-#ui <- fluidPage(theme = shinytheme("lumen"),
+# ui <- fluidPage(theme = shinytheme("lumen"),
 ui <- fluidPage(theme = material,
-  titlePanel("Campbell from Lötschental sites"),
+  titlePanel("Campbell data from Lötschental sites"),
   sidebarLayout(
     sidebarPanel(
       
       # Select type of trend to plot
       selectizeInput(inputId = "site", label = strong("Site"),
-        choices = substr(names(DATA),5,9),
-        selected = "N13Da"),
+        #choices = substr(names(DATA),5,9),
+        choices = unlist(lapply(names(DATA), FUN=extract)),
+        selected = "N08b"),
       
       # Select date range to be plotted
       dateRangeInput("date", strong("Date range"), start = "2020-01-01", end = "2021-07-31",
         min = "2020-01-01", max = "2021-07-31"),
       
-      # Select whether to overlay smooth trend line
-      checkboxInput(inputId = "smoother", label = strong("Overlay smooth trend line"), value = FALSE),
-      
-      # Display only if the smoother is checked
-      conditionalPanel(condition = "input.smoother == true",
-        sliderInput(inputId = "f", label = "Smoother span:",
-          min = 0.01, max = 1, value = 0.25, step = 0.01,
-          animate = animationOptions(interval = 100)),
-        HTML("Higher values give more smoothness.")
-      )
+      # # Select whether to overlay smooth trend line
+      # checkboxInput(inputId = "smoother", label = strong("Overlay smooth trend line"), value = FALSE),
+      # 
+      # # Display only if the smoother is checked
+      # conditionalPanel(condition = "input.smoother == true",
+      #   sliderInput(inputId = "f", label = "Smoother span:",
+      #     min = 0.01, max = 1, value = 0.25, step = 0.01,
+      #     animate = animationOptions(interval = 100)),
+      #   HTML("Higher values give more smoothness.")
+      # )
     ),
     
     # Output: Description, lineplot, and reference
     mainPanel(
-      plotOutput(outputId = "lineplot", height = "300px"),
-      plotOutput(outputId = "ggplot", height = "300px"),
+      plotOutput(outputId = "ggplot.Batt", height = "150px"),
+      plotOutput(outputId = "ggplot.Temp", height = "150px"),
+      plotOutput(outputId = "ggplot.Dendro", height = "250px"),
+      plotOutput(outputId = "ggplot.Sapflow", height = "250px"),
       tags$a(href = "https://www.wsl.ch/en/tree-ring-research/the-loetschental-tree-growth-monitoring-transect.html", "Source: Lötschental transect", target = "_blank")
     )
   )
 )
+
+
+# dygraphOutput(outputId = "dygraph", height = "300px"),
